2efa8ee4380285d7f0cf36307c187359ef562f2f
Matheus Maia <irmadalava@gmail.com>
2024-09-19 16:43:54 -0300

    Painel

---
+++ C++ABSOLUTO/3_FundamentosDasFunções/painel3-1.cpp
@@ -0,0 +1,28 @@
+// Calcula o tamanho de uma casinha de cachorro que possa ser adquirida com o orçamento dado pelo usuário
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	const double COS_PER_SQ_FT = 10.50;
+	double orcamento, area, comprimento;
+
+	cout << "Informe quanto quer gastar com a casinha de cachorro $";
+	cin >> orcamento;
+
+	area = orcamento/COS_PER_SQ_FT;
+	comprimento = sqrt(area);
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+
+	cout << "Por um preço de $" << orcamento << endl
+		<< "Posso lhe construir uma magnífica casinha de cachorro\n"
+		<< "com " << comprimento
+		<< " pés em cada lado. \n";
+
+	return 0;
+}
